8ea5eca0d15dc570ba470043f17afe8f5ace7117
Yogi120 <me.yogesh.pal@gmail.com>
2023-06-22 15:33:30 +0530

    Frst Name

---
+++ User_Registration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Registration Firtname = new Registration();
+            Firtname.FirstName();
+
+        }
+    }
+}
---
+++ User_Registration/Registration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    public class Registration
+    {
+        public void FirstName()
+        {
+            string pattern = "^[A-Z][a-zA-Z]{2,}$";
+            // "^[A-Z][a-zA-Z]{2,}$"
+            string[] Names = { "Yogesh", "Amit", "rohan" };
+
+            Regex myName = new Regex(pattern);
+
+            foreach (string Name in Names)
+            {
+                if (myName.IsMatch(Name) == true)
+                {
+                    Console.WriteLine($"{Name} : is a VALID Name");
+                }
+
+                else 
+                {
+                    Console.WriteLine($"{Name} : is a INVALID!! Name");
+                }
+            }
+        }
+    }
+}
